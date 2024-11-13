a675f28e0625d48508fe9dd15f01bd9dfb688e01
TasticXD <162758302+TasticXD@users.noreply.github.com>
2024-11-13 16:58:19 -0300

    Camera.js

---
+++ Camera.js
@@ -0,0 +1,51 @@
+import {View, StyleSheet, TouchableOpacity,Button} from 'react-native'
+import { CameraView, useCameraPermissions } from 'expo-camera'
+import React, {useEffect, useRef} from 'react'
+import * as MediaLibrary from 'expo-media-library'
+
+
+const Camera = function() {
+    const cameraRef = useRef(null)
+    const [permissions, solicitaPermissions] = useCameraPermissions()
+    const [mediaPermission, requestMediaPermission] = MediaLibrary.usePermissions()
+
+    const tirarFoto = function() {
+    cameraRef.current.takePictureAsync()
+        .then((foto) => MediaLibrary.saveToLibraryAsync(foto.uri))
+        .then()
+        .catch ((err) => console.log(err))
+    }
+    
+    useEffect(()=>{solicitaPermissions(), requestMediaPermission()}, [])
+    return(<View style={styles.container}>
+      <CameraView style={styles.camera} ref={cameraRef}>
+        <View style={styles.buttonContainer}>
+          <Button style={styles.button} title="Tirar Foto" onPress={tirarFoto} />
+         </View>
+      </CameraView>
+    </View>)
+}
+
+function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    camera: {
+      flex: 1,
+    },
+    buttonContainer: {
+      position: 'absolute',
+      bottom: 20,
+      left: 0,
+      right: 0,
+      paddingHorizontal: 16,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
+
+export default Camera
